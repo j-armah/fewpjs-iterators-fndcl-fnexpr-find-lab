057b1e5542f674b3a931a3b38c535489254600cf
februar-y <jarmah241@gmail.com>
2020-12-13 20:51:36 -0500

    done

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin (record) {
+  let result = record.find( i => i.result === "W")
+  if (result) {
+    return result.year
+  }
+}
+
+
+
+// superbowlWin = (record) => {
+//   let result = record.find( record => record.result === "W" )
+//   return !!result ? result.year : undefined
+// }
